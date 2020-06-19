8e25cee4236b952f8deb83993f280c7cee2dffd8
tolerans <scriptkidceo@gmail.com>
2020-06-19 20:52:45 +0300

    master

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+extern crate reqwest;
+fn main() {
+
+   match reqwest::get("https://en.wikipedia.org/wiki/Thread_safety"){
+        Ok(mut response) => {
+            if response.status() == reqwest::StatusCode::Ok {
+                match response.text() {
+                    Ok(text) => println!("Resp text: {}", text),
+                    Err(_) => println!("Could not read resp text!")
+                }
+            } else {
+                println!("Unable to connect!");
+            }
+       }
+       Err(_) => println!("Could not find a site!")
+   }
+}
